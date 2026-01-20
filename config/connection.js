9bc58ca3661ae7eb2d0d1fdd0b3146744e61d482
4drea4 <bakerand@outlook.com>
2026-01-20 12:46:07 -0600

    setting up env and connection.js

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+async function connectDB(){
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to Mongo!');
+    } catch (error) {
+        console.log('Mongo connection error', error.message)
+    }
+}
