3d6c66165ccc3637eb66824f285b9d5f29713115
4drea4 <bakerand@outlook.com>
2026-01-20 13:29:27 -0600

    Task 2 . 6 done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,34 @@
 const express = require('express');
 const router = express.Router();
-
 const jwt = require('jsonwebtoken');
-const User = require('..models/User');
+const User = require('../models/User');
+
 
 // post api users register route
 router.post('/register' , async (req,res) => {
+    try{
+        const {username,email,password} = req.body;
+
+    //validate
     if (!username || !email || !password) {
-        return res.status(400).json({message: "Missing some info, check again!"})
+        return res.status(400).json({message: "Missing some info, check again!"});
     }
-    const userExists = await User.search({email});
+    //does the user exist?
+    const userExists = await User.findOne({email});
     if (userExists) {
         return res.status(400).json({message: 'Use with that info already exists!'});
     }
     const newUser = await User.create({username, email, password});
 
     //201 error
-    res.status(201).json()
+    res.status(201).json(newUser);
 
-
-
-})
-//n this route, take the username, email, and password from the req.body.
-
+} catch (error) {
+    res.status(400).json({
+        message: 'User couldnt be registered',
+        error: error.message
+    });
+}
+});
+module.exports = router;
 // Check if a user with the given email already exists. If so, return a 400 status with an appropriate message.
