a574e2eb1987f7af1391585e20adce7265dd957b
4drea4 <bakerand@outlook.com>
2026-01-20 13:57:01 -0600

    updated auth and will be testing

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,21 @@
 const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
+
+function auth (req,res, next) {
+    let token = req.headers.authorization;
+    if (token) {
+        token = token.split('').pop().trim();
+    }
+    if(!token){
+        return res.status(401).json({message: 'Ran out of chuck e cheese tokens'});
+    }
+    try{
+        const {data} = jwt.verify(token,secret, {maxAge: '15min'});
+        req.user = data;
+        return next ();
+
+    }catch (error) {
+        return res.status(401).json({message:"Token is invalid"});
+    }
+    }
+ module.exports = auth;
